7868dc3c477a9b40aac5dbf0aef10eaf72dde249
BretP <bret.prins@gmail.com>
2017-11-01 23:49:51 -0600

    Udacity Feedback Fixes #2

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -24,7 +24,7 @@ $(function() {
 
     // Testing for RSS feed names.
     it('Names are defined and filled in', function() {
-      for (x = 0; allFeeds.length > x; x++) {
+      for (var x = 0; allFeeds.length > x; x++) {
         expect(allFeeds[x].name).toBeDefined();
         expect(allFeeds[x].name).not.toBe('');
       }
@@ -36,15 +36,16 @@ $(function() {
   describe('The Menu', function() {
 
     //Quick test to see if the menu-hidden class is applied to the body on page load.
-    var bodyState = $("body").hasClass("menu-hidden");
     it('Make sure body has Menu-Hidden Class', function() {
+      var bodyState = $("body").hasClass("menu-hidden");
       expect(bodyState).toBe(true);
     });
 
     //A test setup on the menu button to make sure the menu hides itself properly after multiple clicks.
-    var menuLink = document.querySelector(".menu-icon-link");
+
 
     it('Make sure menu displays when clicked', function() {
+      var menuLink = document.querySelector(".menu-icon-link");
       menuLink.click();
       expect($("body").hasClass("menu-hidden")).toBe(false);
       menuLink.click();
@@ -61,7 +62,7 @@ $(function() {
     });
 
     it('Make sure feed has at least one entry', function() {
-      expect($('.feed > .entry-link > .entry').length).toBeGreaterThan(0);
+      expect($('.feed .entry').length).toBeGreaterThan(0);
     });
   });
 
@@ -69,14 +70,20 @@ $(function() {
   describe('New Feed Selection', function() {
 
     // This test checks to make sure the feed has changed and added content after its loaded.
-    var unloadedFeed = document.querySelector('.feed').innerHTML;
+    var unloadedFeed;
+    var loadedFeed;
+
     beforeEach(function(done) {
-      loadFeed(0, done);
+      loadFeed(0, function() {
+        unloadedFeed = document.querySelector('.feed').innerHTML;
+      });
+      loadFeed(1, function() {
+        loadedFeed = document.querySelector('.feed').innerHTML;
+        done();
+      })
     });
 
-    var loadedFeed;
     it('Make sure feed content has changed', function() {
-      loadedFeed = document.querySelector('.feed').innerHTML;
       expect(unloadedFeed).not.toBe(loadedFeed);
     });
   });
